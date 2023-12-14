9d8a82c808c4365083adf3fced56958c892dd0c9
Nikit Patel <64511084+NIKITPATEL@users.noreply.github.com>
2023-12-14 14:26:28 -0500

    new css

--- src/components/change-password.js
+++ src/components/change-password.js
@@ -1,9 +1,8 @@
 import { useState, useEffect } from 'react'; 
 import { withSessionSsr } from "@/lib/session";
-import { getAllCourseSections } from "@/lib/courseSection";
 import axios from "axios";
-import "@/pages/global.css";
 import React from 'react';
+import "@/pages/global.css";
 
 function ChangePassword() {
     const [currentPassword, setCurrentPassword] = useState("");
@@ -36,31 +35,35 @@ function ChangePassword() {
         const response = await axios.post("/api/change-pass", { currentPassword, newPassword, newPasswordConfirm });
   
         if (response.data.success) {
-          alert("done");
           setChangeSuccessful(true);
           setCurrentPassword("");
           setNewPassword("");
           setNewPasswordConfirm("");
+          setTimeout(() => {
+            setChangeSuccessful(false);
+          }, 5000); // Change success message visibility duration (in milliseconds)
         }
       } catch (error) {
         console.log(error);
         if (error.response && error.response.data && error.response.data.message) {
-          alert(error.response.data.message);
           setServerErrorMessage(error.response.data.message);
         } else {
           setServerErrorMessage("Unknown error occurred");
-          alert("Unknown error occurred");
         }
       }
     };
   
     return (
-        <div className="p-4 flex flex-col items-center">
-          <h3 className="text-xl font-bold mb-4">Change Admin Password</h3>
-          {changeSuccessful && (
-            <h2 className="text-green-500">Password successfully changed</h2>
-          )}
-          <div className="flex flex-col w-64">
+        <div className="p-4 flex flex-col items-center transition-all duration-300 ease-in-out transform">
+          <div className="bg-blue-200 rounded-lg p-6 mb-8 w-72">
+            <h3 className="text-xl font-bold mb-4 text-center">Change Admin Password</h3>
+            {changeSuccessful && (
+              <h2 className="text-green-500 text-center opacity-0">
+                Password successfully changed
+              </h2>
+            )}
+          </div>
+          <div className="flex flex-col w-72">
             <label className="mb-2">
               Current Password
               <input
@@ -92,7 +95,7 @@ function ChangePassword() {
               <p className="text-red-500">{errors.newPass}</p>
             )}
             <button
-              className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
+              className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded mt-4 transition-all duration-300 ease-in-out transform hover:scale-105"
               type="submit"
               onClick={changePassword}
             >
@@ -103,6 +106,4 @@ function ChangePassword() {
       );
   }
 
-  export default ChangePassword;
-
-
+export default ChangePassword;
--- src/components/edit-classes.js
+++ src/components/edit-classes.js
@@ -8,16 +8,10 @@ import { data } from "browserslist";
 import { useRouter } from 'next/navigation'
 
 function EditClasses({ sections }) {
-  const router = useRouter()
-  //const [courseSection, setCourseSection] = useState({data});
-
-  // make a deep copy of sections
+  const router = useRouter();
   const [editedSections, setEditedSections] = useState(JSON.parse(JSON.stringify(sections)));
-
-  console.log("editedSections: ", editedSections);
-
   const [errorMessage, setErrorMessage] = useState("");
-  
+
   const resetDBStats = async (index) => {
     try {
       const response = await axios.post("/api/reset-stats", {
@@ -70,93 +64,92 @@ function EditClasses({ sections }) {
   };
 
   return (
-    <div className="p-4 flex flex-col items-center space-y-5">
-      <h3 className="text-xl font-bold mb-4">Edit Class Sections</h3>
-      {errorMessage && (
-        <h2 className="text-red-500">{errorMessage}</h2>
-      )}
-      {editedSections.map((section, index) => (
-        <div className="flex flex-col w-64" key={section.id}>
-          <label className="mb-2">
-            ID
-            <input
-              className="border border-gray-300 p-1 rounded w-full"
-              value={section.id}
-              disabled
-            />
-          </label>
-          
-          <label className="mb-2">
-            Name
-            <input
-              className="border border-gray-300 p-1 rounded w-full"
-              value={section.name}
-              onChange={(e) => handleInputChange(index, "name", e.target.value)}
-            />
-          </label>
-          <label className="mb-2">
-            Description
-            <input
-              className="border border-gray-300 p-1 rounded w-full"
-              value={section.description}
-              onChange={(e) =>
-                handleInputChange(index, "description", e.target.value)
-              }
-            />
-          </label>
-          {/*
-          <label className="mb-2">
-            URL Path (ex: class-1)
-            <input
-              className="border border-gray-300 p-1 rounded w-full"
-              value={section.path}
-              onChange={(e) => handleInputChange(index, "path", e.target.value)}
-            />
-          </label>
-            */}
-          <div className="flex flex-row justify-between w-full mt-4">
+    
+    <div>
+      <h3 className="text-xl items-center font-bold mx-4 my-4">Edit Class Sections</h3>
+    
+  <div className="p-4 flex flex-row space-x-4 items-center"> 
+  {errorMessage && (
+   
+    <h2 className="text-red-500">{errorMessage}</h2>
+  )}
+  {editedSections.map((section, index) => (
+    <div className="flex flex-col w-80 transition-transform border border-orange-500 p-4 rounded transform hover:scale-105" key={section.id}>
+      <label className="mb-2">
+        ID
+        <input
+          className="border border-gray-300 p-1 rounded w-full"
+          value={section.id}
+          disabled
+        />
+      </label>
+
+      <label className="mb-2">
+        Name
+        <input
+          className="border border-gray-300 p-1 rounded w-full"
+          value={section.name}
+          onChange={(e) => handleInputChange(index, "name", e.target.value)}
+        />
+      </label>
+      <label className="mb-2">
+        Description
+        <input
+          className="border border-gray-300 p-1 rounded w-full"
+          value={section.description}
+          onChange={(e) =>
+            handleInputChange(index, "description", e.target.value)
+          }
+        />
+      </label>
+      <div className="flex justify-between mt-4">
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded w-1/2 mr-2 transition-transform transform hover:scale-105"
+          onClick={() => {deleteSection(index)}}
+        >
+          Delete
+        </button>
+        {editedSections[index].id && (
           <button
-              className="bg-red-500 text-white px-4 py-2 rounded w-1/2 mr-2"
-              onClick={() => {deleteSection(index)}}
-            >
-              Delete
-            </button>
-            {editedSections[index].id && (
-            <button
-               className="bg-red-500 text-white px-4 py-2 rounded w-1/2 ml-2"
-              onClick={() => {
-                resetDBStats(index);
-              }}
-            >
-              Reset Stats
-            </button>
-            )}
-          </div>
-        </div>
-      ))}
-
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
-        type="submit"
-        onClick={save}
-      >
-        Save
-      </button>
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
-        type="submit"
-        onClick={revertChanges}
-      >
-        Revert
-      </button>
-
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
-        onClick={addNewSection}
-      >
-        New Class
-      </button>
+            className="bg-red-500 text-white px-4 py-2 rounded w-1/2 ml-2 transition-transform transform hover:scale-105"
+            onClick={() => {
+              resetDBStats(index);
+            }}
+          >
+            Reset Stats
+          </button>
+        )}
+      </div>
     </div>
+  ))}
+
+  </div>
+
+
+      <div className="flexitems-center mx-4 space-x-4 mt-4">
+        <button
+          className="bg-green-500 text-white px-4 py-2 rounded"
+          type="submit"
+          onClick={save}
+        >
+          Save
+        </button>
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded"
+          type="submit"
+          onClick={revertChanges}
+        >
+          Revert
+        </button>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={addNewSection}
+        >
+          New Class
+        </button>
+      </div>
+    
+  </div>  
   );
 }
 
